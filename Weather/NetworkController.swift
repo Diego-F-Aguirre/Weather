d93c2c3740e52c689123ab7d918c2aa5854fbde5
Diego-F-Aguirre <diego3884@gmail.com>
2016-02-22 21:14:17 -0700

    updates

--- Weather/NetworkController.swift
+++ Weather/NetworkController.swift
@@ -17,18 +17,19 @@ class NetworkController {
     static func searchURLByCity(city: String) -> NSURL {
         
         let escapedCityString = city.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet())
-        
-        return NSURL(string: baseURL + "?q=\(escapedCityString!)" + "&appid=\(API_KEY)")!
+        // http://www.example.com/api/1/weather?q=San%20Francisco&appid=abc1234
+        let urlString = baseURL + "?q=\(escapedCityString!)" + "&appid=\(API_KEY)"
+        return NSURL(string: urlString)!
     }
     
-    static func dataAtURL(url: NSURL, completion:(resultData: NSData?) -> Void) {
+    static func dataAtURL(url: NSURL, completion: (resultData: NSData?) -> Void) {
         
         let session = NSURLSession.sharedSession()
         
-        let dataTask = session.dataTaskWithURL(url) { (data, _, error) -> Void in
+        let dataTask = session.dataTaskWithURL(url) { (data: NSData? , response: NSURLResponse?, error: NSError?) -> Void in
             
-            guard let data = data  else {
-                print(error?.localizedDescription)
+            if let error = error {
+                print(error.localizedDescription)
                 completion(resultData: nil)
                 return
             }
@@ -39,3 +40,24 @@ class NetworkController {
         dataTask.resume()
     }
 }
+
+//let boston = NetworkController.searchURLByCity("Boston")
+//
+//NetworkController.fetchDataAtURL(boston) { data in
+//    if let data = data {
+//        // request was successful and I have data
+//    }
+//}
+
+//func fetchData(url: NSURL, completion: (NSData?, NSURLResponse?, NSError?) -> Void) {
+//    NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: completion).resume()
+//}
+//
+//fetchData(NSURL(string: "http://example.com")!) { _ in
+//    print("Hello World")
+//}
+//
+//NSURL // represents a web URL
+//NSURLRequest // represents a full request to a URL
+//NSURLSessionDataTask // represents a single request and response to a URL
+//NSURLSession // represents a network connection
